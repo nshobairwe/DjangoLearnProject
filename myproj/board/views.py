18f4f94f45449e6e179fb8368fd458127981941b
nshobairwe <witnes.nshobairwe@gmail.com>
2025-11-21 15:57:00 +0300

    first commit

---
+++ myproj/board/views.py
@@ -0,0 +1,20 @@
+from django.shortcuts import render, get_object_or_404
+from .models import Board
+from django.http import Http404
+
+def home(request):
+    boards = Board.objects.all()
+    return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request,pk):
+    try:
+        board = Board.objects.get(pk=pk)
+    except Board.DoesNotExist:
+        raise Http404
+    return render(request, "topics.html", {'board': board})
+
+def new_topic(request,pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'new_topic.html', {'board': board})
+
+   
