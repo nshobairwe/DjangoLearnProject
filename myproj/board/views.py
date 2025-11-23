347503932aaee1e3daad389a499987d36817a5ca
witnes <witnes.nshobairwe@gmail.com>
2025-11-23 12:57:06 +0300

    Advanced topic urls and forms in django

--- myproj/board/views.py
+++ myproj/board/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Board
 from django.http import Http404
+from .models import Topic, Post
+from django.contrib.auth.models import User
+from django.shortcuts import redirect
+from .form import NewTopicForm
 
 def home(request):
     boards = Board.objects.all()
@@ -13,8 +17,22 @@ def board_topics(request,pk):
         raise Http404
     return render(request, "topics.html", {'board': board})
 
-def new_topic(request,pk):
+def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
-    return render(request, 'new_topic.html', {'board': board})
-
-   
+    user = User.objects.first()  # TODO: get the currently logged in user
+    if request.method == 'POST':
+        form = NewTopicForm(request.POST)
+        if form.is_valid():
+            topic = form.save(commit=False)
+            topic.board = board
+            topic.starter = user
+            topic.save()
+            post = Post.objects.create(
+                message=form.cleaned_data.get('message'),
+                topic=topic,
+                created_by=user
+            )
+            return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
+    else:
+        form = NewTopicForm()
+    return render(request, 'new_topic.html', {'board': board, 'form': form})
--- myproj/myproj/settings.py
+++ myproj/myproj/settings.py
@@ -39,6 +39,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+
+    'widget_tweaks',
     "board",
     
     
