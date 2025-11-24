def3d11add3b427e69f6f7ca706ac7974f1808c3
nshobairwe <witnes.nshobairwe@gmail.com>
2025-11-24 14:17:40 +0300

    small changes

--- myproj/board/views.py
+++ myproj/board/views.py
@@ -5,6 +5,7 @@ from .models import Topic, Post
 from django.contrib.auth.models import User
 from django.shortcuts import redirect
 from .form import NewTopicForm
+from django.contrib.auth.decorators import login_required
 
 def home(request):
     boards = Board.objects.all()
@@ -17,6 +18,7 @@ def board_topics(request,pk):
         raise Http404
     return render(request, "topics.html", {'board': board})
 
+@login_required
 def new_topic(request, pk):
     board = get_object_or_404(Board, pk=pk)
     user = User.objects.first()  # TODO: get the currently logged in user
@@ -25,12 +27,12 @@ def new_topic(request, pk):
         if form.is_valid():
             topic = form.save(commit=False)
             topic.board = board
-            topic.starter = user
+            topic.starter = request.user
             topic.save()
             post = Post.objects.create(
                 message=form.cleaned_data.get('message'),
                 topic=topic,
-                created_by=user
+                created_by=request.user
             )
             return redirect('board_topics', pk=board.pk)  # TODO: redirect to the created topic page
     else:
